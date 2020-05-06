e2c649aa81c7c34f25704def59a4b31ae7a08063
iamcosmin <iamcosmin@users.noreply.github.com>
2020-05-06 17:03:05 +0300

    5.0.8 u2
    New Settings Page

--- lib/classes/settings.dart
+++ lib/classes/settings.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'colors.dart';
+import 'cellular.dart';
+import 'group.dart';
+import 'header.dart';
+import 'item.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -10,91 +15,181 @@ class SettingsScreen extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      child: Container(
+        color: backgroundGray,
         child: CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Setări'),
-        ),
-        SliverPadding(
-          padding: MediaQuery.of(context)
-              .removePadding(
-                removeTop: true,
-                removeLeft: true,
-                removeRight: true,
-              )
-              .padding,
-          sliver: SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Material(
-                      borderRadius: BorderRadius.circular(5.0),
-                      color: Colors.black87,
-                      child: Column(
-                        children: <Widget>[
-                          ListTile(
-                            title: Align(
-                                heightFactor: 2,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Mod întunecat',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 19,
-                                    )),
-                              ),
-                              trailing:  CupertinoSwitch(
-                                value: true,
-                                onChanged: null,
-                            ),
-                          ),
-                          Divider(color: Colors.grey),
-                          ListTile(
-                            title: Align(
-                                heightFactor: 2,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Primește versiuni beta',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 19,
-                                    )),
-                              ),
-                              trailing:  CupertinoSwitch(
-                                value: true,
-                                onChanged: null,
-                            ),
-                          ),
-                          ListTile(
-                            title: Align(
-                                heightFactor: 2,
-                                alignment: Alignment.centerLeft,
-                                child: Text('Act. carac. experimentale',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 19,
-                                    )),
-                              ),
-                              trailing:  CupertinoSwitch(
-                                value: false,
-                                onChanged: null,
-                            ),
-                          ),
-                          Divider(color: Colors.grey),
-                          Align(alignment: Alignment.bottomCenter,
-                          child: Text('\n Vanto 5.0.7, 2020050100', style: TextStyle(color: Colors.grey)),
-                          ),
-                          Align(alignment: Alignment.bottomCenter,
-                          child: Text('Ești înscris în programul de testare intern!', style: TextStyle(color: Colors.grey)),
-                          )
-                        ],
-                      )),
-                );
-              },
-              childCount: 1,
+          slivers: <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Settings'),
             ),
-          ),
+            SliverSafeArea(
+              top: false,
+              // This is just a big list of all the items in the settings.
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  <Widget>[
+                    SettingsGroup(<Widget>[
+                      SettingsHeader(),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'Airplane Mode',
+                        iconAssetLabel: 'airplane',
+                        type: SettingsItemType.toggle,
+                      ),
+                      SettingsItem(
+                        label: 'Wi-Fi',
+                        iconAssetLabel: 'wifi',
+                        type: SettingsItemType.modal,
+                        value: 'Airport Free',
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Bluetooth',
+                        iconAssetLabel: 'bluetooth',
+                        type: SettingsItemType.modal,
+                        value: 'On',
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Cellular',
+                        iconAssetLabel: 'cellular',
+                        type: SettingsItemType.modal,
+                        onPress: () => Navigator.push(context, CellularPage.route()),
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Personal Hotspot',
+                        iconAssetLabel: 'hotspot',
+                        type: SettingsItemType.modal,
+                        value: 'Off',
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'VPN',
+                        iconAssetLabel: 'vpn',
+                        type: SettingsItemType.modal,
+                        value: 'Not Connected',
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'Notifications',
+                        iconAssetLabel: 'notifications',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Control Center',
+                        iconAssetLabel: 'control_center',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Do Not Disturb',
+                        iconAssetLabel: 'disturb',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'General',
+                        iconAssetLabel: 'general',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Display & Brightness',
+                        iconAssetLabel: 'display',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Wallpaper',
+                        iconAssetLabel: 'wallpaper',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Sounds & Haptics',
+                        iconAssetLabel: 'sounds',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Siri & Search',
+                        iconAssetLabel: 'siri',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Face ID & Passcode',
+                        iconAssetLabel: 'face_id',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Emergency SOS',
+                        iconAssetLabel: 'sos',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Battery',
+                        iconAssetLabel: 'battery',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Privacy',
+                        iconAssetLabel: 'privacy',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'iTunes & App Store',
+                        iconAssetLabel: 'itunes',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Wallet & Apple Pay',
+                        iconAssetLabel: 'wallet',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                    SettingsGroup(<Widget>[
+                      SettingsItem(
+                        label: 'Accounts & Passwords',
+                        iconAssetLabel: 'accounts',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Contacts',
+                        iconAssetLabel: 'contacts',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                      SettingsItem(
+                        label: 'Calendar',
+                        iconAssetLabel: 'calendar',
+                        type: SettingsItemType.modal,
+                        hasDetails: true,
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
